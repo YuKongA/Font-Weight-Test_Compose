80c0be9347146d24431bfc4490a1a74c0bafcb36
余空 <70465933+YuKongA@users.noreply.github.com>
2024-07-10 20:06:25 +0800

    app: fix HomeView verticalScroll

--- composeApp/src/commonMain/kotlin/ui/HomeView.kt
+++ composeApp/src/commonMain/kotlin/ui/HomeView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,7 +26,8 @@ import ui.components.CardView
 
 @Composable
 fun HomeView() {
-    Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+    val scrollState = rememberScrollState()
+    Column(modifier = Modifier.padding(horizontal = 20.dp).verticalScroll(scrollState)) {
         Text(
             text = stringResource(Res.string.font_weight),
             modifier = Modifier.padding(bottom = 8.dp),
